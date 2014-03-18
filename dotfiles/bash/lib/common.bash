e08522bea309afb07490f380a540ef43edad3673
Daniel Moore <polaris@northhorizon.net>
2014-03-18 08:57:32 -0400

    adding parallel bash function

--- dotfiles/bash/lib/common.bash
+++ dotfiles/bash/lib/common.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # don't put duplicate lines in the history. See bash(1) for more options
-export HISTCONTROL=ignoredups
-# ... and ignore same sucessive entries.
 export HISTCONTROL=ignoreboth
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
-shopt -s checkwinsize
+shopt -s checkwinsize extglob
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
@@ -21,3 +19,11 @@ export CLICOLOR=1
 if [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
 fi
+
+function parallel {
+  trap $'kill $(jobs -l | cut -f 2 -d \' \')' SIGINT
+
+  for cmd in "$@"; do
+    $cmd &
+  done
+}
