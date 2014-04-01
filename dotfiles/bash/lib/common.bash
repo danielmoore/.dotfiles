585a1c5cca1c924ab1fb9ad3a516bba876152507
Daniel Moore <polaris@northhorizon.net>
2014-04-01 09:05:18 -0400

    adding globstar to bash shopt

--- dotfiles/bash/lib/common.bash
+++ dotfiles/bash/lib/common.bash
@@ -5,7 +5,7 @@ export HISTCONTROL=ignoreboth
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
-shopt -s checkwinsize extglob
+shopt -s checkwinsize extglob globstar
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
