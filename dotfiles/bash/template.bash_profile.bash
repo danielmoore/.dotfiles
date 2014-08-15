2417e7e2eaca66ad3dad91d9d9129a63dcf354b5
Daniel Moore <polaris@northhorizon.net>
2014-08-15 08:27:39 +0100

    minor updates

--- dotfiles/bash/template.bash_profile.bash
+++ dotfiles/bash/template.bash_profile.bash
@@ -2,5 +2,8 @@
 
 export PROFILE_DIR=%PROFILE_DIR%
 
-source $PROFILE_DIR/dotfiles/bash/$(uname | tr '[A-Z]' '[a-z]').bashrc.bash
-source $PROFILE_DIR/dotfiles/bash/$(uname | tr '[A-Z]' '[a-z]').bash_profile.bash
+source $PROFILE_DIR/dotfiles/bash/$(uname | tr '[A-Z]' '[a-z]').bashrc.bash || \
+source $PROFILE_DIR/dotfiles/bash/linux.bashrc.bash
+
+source $PROFILE_DIR/dotfiles/bash/$(uname | tr '[A-Z]' '[a-z]').bash_profile.bash || \
+source $PROFILE_DIR/dotfiles/bash/linux.bash_profile.bash
