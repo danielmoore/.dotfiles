c2d420a49db0beab4c7a27a472712aee8e53ad82
Daniel Moore <daniel.s.moore@capitalone.com>
2015-10-01 13:18:50 -0400

    Enable direct deletion of merged branches in clean-branches

--- dotfiles/git/lib/clean-branches.sh
+++ dotfiles/git/lib/clean-branches.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 onerror() {
   line=$1
@@ -9,15 +9,18 @@ onerror() {
 trap 'onerror $LINENO' ERR
 
 usage() {
-  cat << EOF
+  cat << EOS
 usage: $0 [options]
 
 Options:
-  -o <offset>  Specify the cutoff offset for trashing a branch. Default: 14d
+  -t           Move old branches to trash/ (see -a)
+  -a <time>    Specify the minimum age for trashing a branch. Implies -t. Default: 14d
+  -e           Remove all branches under trash/
+  -m           Remove all merged branches
   -r           Clean remote branches instead of local branches
   -w           Perform a "what if" dry-run. Only prints out actions that would have been performed.
   -h           Shows this message and quits.
-EOF
+EOS
 }
 
 branchWhitelist=.git-branch-whitelist
@@ -25,13 +28,17 @@ branchWhitelist=.git-branch-whitelist
 cutoffOffset=14d
 branchOpts=
 emptyTrash=false
+deleteMerged=false
 whatIf=false
 remote=false
+trashOldBranches=false
 
-while getopts 'o:rewh' OPTION; do case $OPTION in
+while getopts 'hemo:wr' OPTION; do case $OPTION in
   h) usage; exit;;
   e) emptyTrash=true;;
-  o) cutoffOffset=$OPTARG;;
+  m) deleteMerged=true;;
+  a) cutoffOffset=$OPTARG;&
+  t) trashOldBranches=true;;
   w) whatIf=true;;
   r) remote=true;;
   ?) usage; exit -1;;
@@ -46,17 +53,17 @@ fi
 
 cutoff=$(date -v -${cutoffOffset} +%s)
 
-case "$emptyTrash $whatIf" in
-  'true false')  verb='Deleting';;
-  'true true')   verb='Would delete';;
-  'false false') verb='Moving';;
-  'false true')  verb='Would move';;
-esac
+if $whatIf; then
+  deleteVerb='Would delete'
+  moveVerb='Would move'
+else
+  deleteVerb='Deleting'
+  moveVerb='Moving'
+fi
 
-sources=("<(git branch $branchOpts)")
 egrepOpts=('-e (^|/)trash/')
 if ! $emptyTrash; then
-  sources+=("<(git branch $branchOpts --merged $masterBranch)")
+  sources+=("<()")
   # don't include pointers, i.e. foo -> bar
   egrepOpts+=('-v' "-e ^${masterBranch}\$" '-e \->')
 
@@ -65,44 +72,77 @@ if ! $emptyTrash; then
   fi
 fi
 
-eval "sort -m ${sources[@]}" | cut -c 3- | egrep ${egrepOpts[@]} | {
-  while read branch; do
-    if $remote; then
-      remoteName=${branch%%/*}
-      remoteBranch=${branch#*/}
-      oldBranch=$remoteBranch
-      newBranch=trash/$remoteBranch
-    else
-      oldBranch=$branch
-      newBranch=trash/$branch
-    fi
+moveBranch() {
+  removteName=$1
+  oldBranch=$2
+  newBranch=$3
+  reason=$4
 
-    if $emptyTrash; then
-      echo "$verb $branch"
+  echo "$moveVerb $oldBranch -> $newBranch ($reason)"
 
-      if $whatIf; then continue; fi
+  if $whatIf; then return; fi
 
+  if $remote; then
+    git push $remoteName "$oldBranch:refs/heads/$newBranch"
+    git push --delete $remoteName $remoteBranch
+  else
+    git branch $newBranch $oldBranch
+    git branch -D $oldBranch
+  fi
+}
+
+deleteBranch() {
+  remoteName=$1
+  oldBranch=$2
+  reason=$3
+
+  echo $deleteVerb $remoteName $oldBranch
+
+  if $whatIf; then return; fi
+
+  if [[ -z $remoteName ]]; then
+    git branch -D $oldBranch
+  else
+    git push --delete $remoteName $oldBranch
+  fi
+}
+
+if $deleteMerged; then
+  git branch $branchOpts --merged $masterBranch | cut -c 3- | egrep -v -e "^${masterBranch}\$" -e '\->' | {
+    while read branch; do
       if $remote; then
-        git push --delete $remoteName $remoteBranch
+        remoteName=${branch%%/*}
+        branchName=${branch#*/}
       else
-        git branch -D $branch
-      fi 
-    else
-      branchDate=$(git show -s --format=%ct $branch)
+        branchName=$branch
+      fi
 
-      if [ "$branchDate" -lt "$cutoff" ]; then
-        echo "$verb $oldBranch -> $newBranch (last commit was on $(date -j -f %s $branchDate))"
+      deleteBranch "$remoteName" $branchName 'merged'
+    done
+  }
+fi
 
-        if $whatIf; then continue; fi
+if $trashOldBranches || $emptyTrash; then
+  git branch $branchOpts --no-merged $masterBranch | cut -c 3- | egrep ${egrepOpts[@]} | {
+    while read branch; do
+      if $remote; then
+        remoteName=${branch%%/*}
+        oldBranch=${branch#*/}
+      else
+        oldBranch=$branch
+      fi
 
-        if $remote; then 
-          git push $remoteName "$branch:refs/heads/$newBranch"
-          git push --delete $remoteName $remoteBranch
-        else
-          git branch $newBranch $branch
-          git branch -D $branch
+      newBranch=trash/$oldBranch
+
+      if $emptyTrash; then
+        deleteBranch "$remoteName" $oldBranch 'in trash'
+      else
+        branchDate=$(git show -s --format=%ct $branch)
+
+        if [ "$branchDate" -lt "$cutoff" ]; then
+          moveBranch "$remoteName" $oldBranch $newBranch "last commit was on $(date -j -f %s $branchDate)"
         fi
       fi
-    fi
-  done
-}
+    done
+  }
+fi
