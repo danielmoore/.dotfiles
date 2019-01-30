247a7dcde3136d08ad1596483493d12733c30b01
Daniel Moore <daniel.s.moore@capitalone.com>
2019-01-29 22:24:53 -0500

    Enable powerline on MacOS

--- dotfiles/bash/darwin.bashrc.bash
+++ dotfiles/bash/darwin.bashrc.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#export XDG_CONFIG_DIRS=$PROFILE_DIR
+export POWERLINE_CONFIG_PATHS="$PROFILE_DIR/powerline"
+export POWERLINE_ROOT="$(pip3 show powerline-status | grep -e '^Location:' | cut -f2 -d ' ')/powerline"
+
 source $PROFILE_DIR/dotfiles/bash/lib/index.bash
 source $PROFILE_DIR/dotfiles/bash/aliases/darwin.bash
 
--- dotfiles/bash/lib/prompt.bash
+++ dotfiles/bash/lib/prompt.bash
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# this depends on ./git.bash being sourced already.
+if [[ -n "$POWERLINE_ROOT" ]]; then
+  powerline-daemon -q
+  POWERLINE_BASH_CONTINUATION=1
+  POWERLINE_BASH_SELECT=1
+  source "$POWERLINE_ROOT/bindings/bash/powerline.sh"
+else
+  # this depends on ./git.bash being sourced already.
 
-env=$(cat ~/.env 2>/dev/null)
+  env=$(cat ~/.env 2>/dev/null)
 
-case $env in
-  qa)       hostColor='01;33'    ;; # bold yellow
-  uat)      hostColor='01;33'    ;; # bold yellow
-  prod)     hostColor='41;01;97' ;; # bg-red, bold white
-  *)        hostColor='01;32'    ;; # bold green
-esac
+  case $env in
+    qa)       hostColor='01;33'    ;; # bold yellow
+    uat)      hostColor='01;33'    ;; # bold yellow
+    prod)     hostColor='41;01;97' ;; # bg-red, bold white
+    *)        hostColor='01;32'    ;; # bold green
+  esac
 
-export PROMPT_COMMAND=$'__git_ps1 \'\e[${hostColor}m\u@\h\e[0m \xE2\x96\xB8 \e[00;34m\w\e[0m\' \'\n\! \$ \' \' %s\''
+  export PROMPT_COMMAND=$'__git_ps1 \'\e[${hostColor}m\u@\h\e[0m \xE2\x96\xB8 \e[00;34m\w\e[0m\' \'\n\! \$ \' \' %s\''
+fi
